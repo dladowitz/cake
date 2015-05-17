83c904540f7f33db6a8742c813641b72e5975ba3
David Ladowitz <david@ladowitz.com>
2015-05-16 17:18:05 -0700

    adding new images

--- seed.sql
+++ seed.sql
@@ -7,12 +7,12 @@
 
 -- //create locations
 INSERT INTO locations(name, img_url) values('Tradecraft', 'http://s3-media2.fl.yelpcdn.com/bphoto/ISimnz4_yTvuPaa-aOwDMA/l.jpg');
-INSERT INTO locations(name, img_url) values('Jackson Square', 'http://tripwhat.com/img/travel/item:-9223372036852163167:photos/831eeca229abe5ba7e4a421bfd7aff50-450-190');
-INSERT INTO locations(name, img_url) values('Yo Yos', 'http://tripwhat.com/img/travel/item:-9223372036852157418:photos/ffffbeb52cb7257f990f29f2ff9d091a-450-190');
-INSERT INTO locations(name, img_url) values('Grumpys Pub', 'http://tripwhat.com/img/travel/item:-9223372036850039777:photos/2ef7c43f7efd71204dff40409ca274bf-450-190');
+INSERT INTO locations(name, img_url) values('Jackson Place Cafe', 'http://s3-media2.fl.yelpcdn.com/bphoto/wnzCFQmFCelHRvtMDSBQuQ/l.jpg');
+INSERT INTO locations(name, img_url) values('Yo Yos', 'http://s3-media1.fl.yelpcdn.com/bphoto/2rvvRVjBH6HFKq2OihuB0g/l.jpg');
+INSERT INTO locations(name, img_url) values('Grumpys Pub', 'http://s3-media1.fl.yelpcdn.com/bphoto/qb6rta1fa41Zif2Ni_Relw/l.jpg');
 INSERT INTO locations(name, img_url) values('Cafe Me', 'http://tripwhat.com/img/travel/item:-9223372036852286085:photos/8daa4557f8438379735fe1de5c12bd33-450-190');
 INSERT INTO locations(name, img_url) values('Reveille Coffee', 'http://s3-media4.fl.yelpcdn.com/bphoto/hVRz34RKipOGxAj2KH3Aww/l.jpg');
-INSERT INTO locations(name, img_url) values('Brioche Bakery', 'http://tripwhat.com/img/travel/item:-9223372036850039629:photos/7dfa153511e6b7537ab3c5747b63f9e8-450-190');
+INSERT INTO locations(name, img_url) values('Brioche Bakery', 'http://s3-media1.fl.yelpcdn.com/bphoto/CPo8fHYCkiCgtf5YAAxREQ/l.jpg');
 INSERT INTO locations(name, img_url) values('Breaking Break', 'http://s3-media1.fl.yelpcdn.com/bphoto/efufJEZ0aSItXe76epfNZg/l.jpg');
 
 INSERT INTO deals(name, location_id, birthday_deal, message) values('Free Tuition', 1, true, 'Free tuition for your birthday - a $14K value!');
